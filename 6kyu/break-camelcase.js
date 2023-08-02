47d91d9f42b653767b5f6cfe79ceb251815b9aad
Adam Smith <adamps724@gmail.com>
2023-08-01 22:56:02 -0500

    added new 6 kyu codewar challenges done over the last month

---
+++ 6kyu/break-camelcase.js
@@ -0,0 +1,14 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+/*My Solution*/
+
+function solution(string) {
+    return string.split('').map(c=>{
+      return c === c.toUpperCase() ? ` ${c}` : c
+    }).join('')
+}
