f1e9896cd67c14bf319e2bccb3e49d5d085c5500
Adam Smith <adamps724@gmail.com>
2023-08-09 19:25:33 -0500

    added new 6kyu codewars solution

---
+++ 6kyu/split-strings.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+
+/*My Solution*/
+
+function solution(str){
+let result = []
+if (str.length % 2 !== 0){
+  str = str + '_'
+}
+for(let i = 0; i < str.length; i += 2){
+  result.push(str[i]+str[i+1])
+}
+return result
+}
