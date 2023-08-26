319f83f79f267a81edcfab8d5c7fed20c61eb24b
Adam Smith <adamps724@gmail.com>
2023-08-25 23:03:54 -0500

    added new 7kyu codewars solution

---
+++ 7kyu/taill-swap.js
@@ -0,0 +1,11 @@
+// You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle (but not at beginning or end). The length of the strings, before and after the colon, are random.
+
+// Your job is to return a list of two strings (in the same order as the original list), but with the characters after each colon swapped.
+
+/*My Solution*/
+
+function tailSwap(arr) {
+  let newArr = arr
+  .map(string => string.split(':'))
+  return [newArr[0][0]+':'+newArr[1][1], newArr[1][0]+':'+newArr[0][1]];
+}
