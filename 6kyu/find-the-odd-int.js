7f1f753c2ddd3317c79e0e702b25b4003741431f
Adam Smith <adamps724@gmail.com>
2024-01-18 20:19:51 -0600

    added new codewars solution

---
+++ 6kyu/find-the-odd-int.js
@@ -0,0 +1,34 @@
+
+
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+
+
+
+
+/*My Solution*/
+
+function findOdd(a) {
+  let odd = [];
+  for (let i = 0; i < a.length; i++) {
+    let count = 0;
+    for (let j = 0; j < a.length; j++) {
+      if (a[i] === a[j]) {
+        count++;
+      }
+    }
+    if (count % 2 !== 0) {
+      return a[i]
+    }
+  }
+  return odd;
+}
